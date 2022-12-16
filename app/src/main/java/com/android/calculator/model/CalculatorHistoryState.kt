75b41f8081c9a2cbc91c649bdb6cbf9e197ffdf6
daniel-iroka <danieliroka67@gmail.com>
2022-12-16 22:41:40 +0100

    Deleted our CalculatorHistoryState but might add it back later.

--- app/src/main/java/com/android/calculator/model/CalculatorHistoryState.kt
+++
@@ -1,7 +0,0 @@
-package com.android.calculator.model
-
-data class CalculatorHistoryState(
-    val primaryState : String = "10 × 9",
-    val secondaryState : String = "90",
-    val time : String = "3 days ago"
-)
